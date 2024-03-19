6df665408fa51786c7c6e31519aba7a5d4ad0f79
Iñigo Garcia Ugarte <garcia.127407@e.unavarra.es>
2024-03-19 10:37:04 +0100

    ReSepFormer_Librimix

---
+++ package/.ipynb_checkpoints/dataPrep-checkpoint.py
@@ -0,0 +1,86 @@
+def dataio_prep(hparams):
+    """Creates data processing pipeline"""
+
+    # 1. Define datasets
+    train_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["train_data"],
+        replacements={"data_root": hparams["data_folder"]},
+    )
+
+    valid_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["valid_data"],
+        replacements={"data_root": hparams["data_folder"]},
+    )
+
+    test_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["test_data"],
+        replacements={"data_root": hparams["data_folder"]},
+    )
+
+    datasets = [train_data, valid_data, test_data]
+
+    # 2. Provide audio pipelines
+
+    @sb.utils.data_pipeline.takes("mix_wav")
+    @sb.utils.data_pipeline.provides("mix_sig")
+    def audio_pipeline_mix(mix_wav):
+        mix_sig = sb.dataio.dataio.read_audio(mix_wav)
+        return mix_sig
+
+    @sb.utils.data_pipeline.takes("s1_wav")
+    @sb.utils.data_pipeline.provides("s1_sig")
+    def audio_pipeline_s1(s1_wav):
+        s1_sig = sb.dataio.dataio.read_audio(s1_wav)
+        return s1_sig
+
+    @sb.utils.data_pipeline.takes("s2_wav")
+    @sb.utils.data_pipeline.provides("s2_sig")
+    def audio_pipeline_s2(s2_wav):
+        s2_sig = sb.dataio.dataio.read_audio(s2_wav)
+        return s2_sig
+
+    if hparams["num_spks"] == 3:
+
+        @sb.utils.data_pipeline.takes("s3_wav")
+        @sb.utils.data_pipeline.provides("s3_sig")
+        def audio_pipeline_s3(s3_wav):
+            s3_sig = sb.dataio.dataio.read_audio(s3_wav)
+            return s3_sig
+
+    if hparams["use_wham_noise"]:
+
+        @sb.utils.data_pipeline.takes("noise_wav")
+        @sb.utils.data_pipeline.provides("noise_sig")
+        def audio_pipeline_noise(noise_wav):
+            noise_sig = sb.dataio.dataio.read_audio(noise_wav)
+            return noise_sig
+
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_mix)
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_s1)
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_s2)
+    if hparams["num_spks"] == 3:
+        sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_s3)
+
+    if hparams["use_wham_noise"]:
+        print("Using the WHAM! noise in the data pipeline")
+        sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_noise)
+
+    if (hparams["num_spks"] == 2) and hparams["use_wham_noise"]:
+        sb.dataio.dataset.set_output_keys(
+            datasets, ["id", "mix_sig", "s1_sig", "s2_sig", "noise_sig"]
+        )
+    elif (hparams["num_spks"] == 3) and hparams["use_wham_noise"]:
+        sb.dataio.dataset.set_output_keys(
+            datasets,
+            ["id", "mix_sig", "s1_sig", "s2_sig", "s3_sig", "noise_sig"],
+        )
+    elif (hparams["num_spks"] == 2) and not hparams["use_wham_noise"]:
+        sb.dataio.dataset.set_output_keys(
+            datasets, ["id", "mix_sig", "s1_sig", "s2_sig"]
+        )
+    else:
+        sb.dataio.dataset.set_output_keys(
+            datasets, ["id", "mix_sig", "s1_sig", "s2_sig", "s3_sig"]
+        )
+
+    return train_data, valid_data, test_data
